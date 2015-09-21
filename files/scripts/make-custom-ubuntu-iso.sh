d0cc7fa81f028e26e75d29b1f07b3006ad64bc66
Paul Gomersbach <p.gomersbach@rely.nl>
2015-09-21 12:40:42 +0000

    change to new ubuntu version

--- files/scripts/make-custom-ubuntu-iso.sh
+++ files/scripts/make-custom-ubuntu-iso.sh
@@ -2,25 +2,25 @@
 apt-get install genisoimage
 cd /tmp
 mkdir original-iso custom-iso
-if [ ! -f /tmp/ubuntu-12.04.4-server-amd64.iso ]; then
+if [ ! -f /tmp/ubuntu-14.04.3-server-amd64.iso ]; then
     echo "Iso image not found, downloading"
-    wget http://releases.ubuntu.com/12.04/ubuntu-12.04.4-server-amd64.iso
+    wget http://releases.ubuntu.com/14.04/ubuntu-14.04.3-server-amd64.iso
 fi
-mount -o loop ubuntu-12.04.4-server-amd64.iso ./original-iso
+mount -o loop /tmp/ubuntu-14.04.3-server-amd64.iso ./original-iso
 cp -r ./original-iso/* ./custom-iso/
 cp -r ./original-iso/.disk/ ./custom-iso/
-wget https://puppet-autoinstall.googlecode.com/git/files/preseed-ubuntu-slave.seed -O ./custom-iso/preseed/seed.seed
+wget https://github.com/pgomersbach/puppet-autoinstall/raw/master/files/preseed-ubuntu-slave.seed -O ./custom-iso/preseed/seed.seed
 umount ./original-iso/
 
 cat > ./custom-iso/isolinux/menu.cfg << EOF
 label custom1
-menu label ^Install Ubuntu 12.04, apply puppet manifest add to foreman
+menu label ^Install Ubuntu 14.04, apply puppet manifest add to foreman
 kernel /install/vmlinuz
 append file=/cdrom/preseed/seed.seed initrd=/install/initrd.gz locale=en_US auto=true console-setup/ask_detect=false keyboard-configuration/layoutcode=us
 
 EOF
 
-genisoimage -R -J -l -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -z -iso-level 4 -c isolinux/isolinux.cat -o ubuntu-12.04.4-custom-amd64.iso custom-iso/
+genisoimage -R -J -l -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -z -iso-level 4 -c isolinux/isolinux.cat -o ubuntu-14.04.3-custom-amd64.iso custom-iso/
 rm -rf original-iso custom-iso
-echo "The custom iso image: /tmp/ubuntu-12.04.4-custom-amd64.iso"
+echo "The custom iso image: /tmp/ubuntu-14.04.3-custom-amd64.iso"
 cd -
