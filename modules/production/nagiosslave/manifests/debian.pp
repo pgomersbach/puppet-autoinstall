56e559edb9436173e96203926caffbba3997a5d1
jeroen-vonk <j.vonk@rely.nl>
2016-03-17 14:36:26 +0100

    Update debian.pp

--- modules/production/nagiosslave/manifests/debian.pp
+++ modules/production/nagiosslave/manifests/debian.pp
@@ -440,6 +440,15 @@ $rndrestart = fqdn_rand(30)
     notify  => Exec['update-nagiosslave.conf'],
   }
   
+    file { '/usr/lib/nagios/plugins/check_cpu_snmp.bash':
+    mode    => '0755',
+    owner   => root,
+    group   => root,
+    source  => 'puppet:///modules/nagiosslave/check_cpu_snmp.bash',
+    require => File['/etc/nagios-plugins/plugins.d'],
+    notify  => Exec['update-nagiosslave.conf'],
+  }
+  
   if file_exists("/etc/puppet/modules/production/nagiosslave/files/checks/${fqdn}.conf") == 1 {
     file { "/etc/nagios-plugins/plugins.d/${fqdn}.conf":
       mode    => '0655',
